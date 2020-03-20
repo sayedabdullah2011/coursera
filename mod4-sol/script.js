4cc038a8deb39e0826e5f9cbf1dda6e7d214a625
Sayed Abdullah Qutb <s.abdullahqutb@gmail.com>
2020-03-20 04:06:05 +0300

    Commented the code

--- mod4-sol/script.js
+++ mod4-sol/script.js
@@ -1,3 +1,12 @@
+/*
+  MODULE 4 Solution
+  main script javascript code
+  author: Sayed Abdullah Qutb
+  date: 20/03/2020
+  version: 1.0
+ */
+
+
 // *******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
@@ -30,35 +39,16 @@ WARNING!!! WARNING!!!
 */
 
 // STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
-
 (function() {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using the 'speak' method or either helloSpeaker's or byeSpeaker's
-// 'speak' method.
-// See Lecture 50, part 1
-/* fill in parts of the 'for' loop to loop over names array */
 for (prop in names ) {
   
   // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[prop].charAt(0).toLowerCase();
 
   // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
   if (firstLetter == "j") {
     byeSpeaker.speak(names[prop]);
   } else {
